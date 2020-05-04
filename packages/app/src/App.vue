4e4b588cd1e96385d9f82016190a35dd4fb2829c
Anton <anton@ju60.de>
2020-05-04 19:42:10 +0200

    update UpdateBar

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -3,14 +3,14 @@
     <NavBar />
     <router-view />
     <Footer />
-    <Snackbar :show="updateExists" text="Es steht ein neues Update bereit" action="Installieren" @click="refreshApp" />
+    <UpdateBar />
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
 import Footer from '@/components/Footer.vue';
-import Snackbar from '@/components/Snackbar.vue';
+import UpdateBar from '@/components/UpdateBar.vue';
 
 export default {
   name: 'App',
@@ -18,44 +18,7 @@ export default {
   components: {
     NavBar,
     Footer,
-    Snackbar,
-  },
-
-  data() {
-    return {
-      refreshing: false,
-      updateExists: false,
-      registration: null,
-    };
-  },
-
-  created() {
-    // Listen for swUpdated event and display refresh snackbar as required.
-    document.addEventListener('swUpdated', this.showRefreshUI, { once: true });
-
-    // Refresh all open app tabs when a new service worker is installed.
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      if (this.refreshing) return;
-      this.refreshing = true;
-      window.location.reload(true);
-    });
-  },
-
-  methods: {
-    showRefreshUI(e) {
-      // Display a snackbar inviting the user to refresh/reload the app due
-      // to an app update being available.
-      // The new service worker is installed, but not yet active.
-      // Store the ServiceWorkerRegistration instance for later use.
-      this.registration = e.detail;
-      this.updateExists = true;
-    },
-    refreshApp() {
-      this.updateExists = false;
-      // Protect against missing registration.waiting.
-      if (!this.registration || !this.registration.waiting) { return; }
-      this.registration.waiting.postMessage('skipWaiting');
-    },
+    UpdateBar,
   },
 };
 </script>
---
+++ packages/app/src/components/UpdateBar.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: 'UpdateBar',
+
+  data() {
+    return {
+      refreshing: false,
+      registration: null,
+    };
+  },
+
+  created() {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener('swUpdated', this.showRefreshUI, { once: true });
+
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (this.refreshing) return;
+      this.refreshing = true;
+      window.location.reload();
+    });
+  },
+
+  methods: {
+    showRefreshUI(e) {
+      // Display a snackbar inviting the user to refresh/reload the app due
+      // to an app update being available.
+      // The new service worker is installed, but not yet active.
+      // Store the ServiceWorkerRegistration instance for later use.
+      this.registration = e.detail;
+      this.$buefy.snackbar.open({
+        message: 'Es steht ein neues Update bereit',
+        type: 'is-success',
+        position: 'is-bottom',
+        actionText: 'Installieren',
+        indefinite: true,
+        onAction: () => {
+          // Protect against missing registration.waiting.
+          if (!this.registration || !this.registration.waiting) {
+            return;
+          }
+          this.registration.waiting.postMessage('skipWaiting');
+        },
+      });
+    },
+  },
+};
+</script>
