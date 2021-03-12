646c4bb48bb188b2bd62bc56392b4f651c890f38
anbraten <anton@ju60.de>
2021-03-12 19:49:41 +0000

    Deploying to deployment from @ anbraten/obelix@232e287c561e841052d22f5468761a6b2403f6f1 🚀

--- env-config.js
+++ env-config.js
@@ -1,8 +1,6 @@
 window._env_ = {
   APP_BACKEND_URL: 'https://api-obelix.ju60.de',
-  APP_MATOMO_SITE: '6',
   APP_OIDC_CLIENT_ID: 'obelix',
-  APP_MATOMO_URL: '//stats.rzab.de/',
   APP_SENTRY_DSN: 'https://cd274507ebe7400a87b0b202452f18d2@error.rzab.de/6',
   APP_TITLE: 'Obelix',
   APP_OIDC_URL: 'https://auth.rzab.de/auth/realms/rzab',
